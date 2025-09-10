306a49209b1124e82c8eb385f668e47b9afd5097
baileybt3 <139825786+baileybt3@users.noreply.github.com>
2025-09-10 11:53:31 -0400

    feat: add basic enemy spawner

---
+++ Assets/_Scripts/Combat/EnemySpawner.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    public float spawnInterval = 3f;
+    private float timer;
+    public int maxEnemies = 5;
+    
+
+    
+    void Start()
+    {
+        timer = spawnInterval;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+
+        int currentEnemies = GameObject.FindGameObjectsWithTag("Enemy").Length;
+
+        if (timer <= 0 && currentEnemies < maxEnemies)
+        {
+            Instantiate(enemyPrefab, transform.position, Quaternion.identity);
+            timer = spawnInterval;
+        }
+    }
+
+    
+}
