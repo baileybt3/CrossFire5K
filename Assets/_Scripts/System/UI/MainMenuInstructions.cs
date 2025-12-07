53a57f1663c6d64aee104a4ce3cf8579b5ce6025
baileybt3 <139825786+baileybt3@users.noreply.github.com>
2025-12-06 23:13:11 -0500

    feat: add instructions panel

---
+++ Assets/_Scripts/System/UI/MainMenuInstructions.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuInstructions : MonoBehaviour
+{
+
+    [Header("references")]
+    [SerializeField] private GameObject instructionsPanel;
+
+    [SerializeField] private bool showOnce = true;
+
+    private const string HasSeenKey = "HasSeenInstructions";
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        bool hasSeen = PlayerPrefs.GetInt(HasSeenKey, 0) == 1;
+
+        if(instructionsPanel != null)
+        {
+            instructionsPanel.SetActive(!showOnce || !hasSeen);
+        }
+    }
+
+    // Okay button
+    public void OnOkClicked()
+    {
+        if(instructionsPanel != null)
+        {
+            instructionsPanel.SetActive(false);
+        }
+
+        if (showOnce)
+        {
+            PlayerPrefs.SetInt(HasSeenKey, 1);
+            PlayerPrefs.Save();
+        }
+
+        if(AudioManager.Instance != null)
+        {
+            AudioManager.Instance.PlayUIClick();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
